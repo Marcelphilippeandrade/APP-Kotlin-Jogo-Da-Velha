26cc492cc6aea488e95a71297a705a57b63e1c60
Marcel Philippe Abreu Andrade <marcelpaa@gmail.com>
2020-06-17 11:04:44 -0300

    Criação do arquivo README.md

---
+++ README.md
@@ -0,0 +1,12 @@
+# APP Android - JOGO DA VELHA
+Este aplicativo é um jogo da Velha. Ele foi desenvolvido utilizando a linguagem Kotlin.
+
+# Objetivo:
+Construir um jogo da Velha.
+
+# Tecnologias utilizadas: 
+* Kotlin
+
+:grin: :lollipop: :computer:
+
+
---
+++ app/src/main/java/br/com/marcelphilippe/jogodavelha/AnimacaoGanhadorActivity.kt
@@ -0,0 +1,12 @@
+package br.com.marcelphilippe.jogodavelha
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AnimacaoGanhadorActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_animacao_ganhador)
+    }
+}
